6d9545336e413ef54d7ec00375688749a951a744
Carlos Jeurissen <1038267+CarlosJeurissen@users.noreply.github.com>
2023-06-08 11:21:44 +0200

    Update tests for additional clarity and with lab color fixes

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -12,7 +12,7 @@ const fs = require('node:fs');
 const postcss = require('postcss');
 const postcssPluginsPreset = require('..');
 
-const validInputCss = fs.readFileSync('./__tests__/valid-input.css', 'utf8');
+const inputCss = fs.readFileSync('./__tests__/input.css', 'utf8');
 const validOutputCss = fs.readFileSync('./__tests__/valid-output.css', 'utf8');
 const validOutputMinifiedCss = fs.readFileSync('./__tests__/valid-output-minified.css', 'utf8');
 
@@ -20,7 +20,7 @@ describe('The output matches expectations', () => {
   test('normal output', () => postcss(postcssPluginsPreset({
     next: true,
     minify: false,
-  })).process(validInputCss, {
+  })).process(inputCss, {
     from: undefined,
   }).then(({ css }) => {
     fs.writeFileSync('./__tests__/actual-output.css', css);
@@ -39,7 +39,7 @@ describe('The output matches expectations', () => {
   test('minified output', () => postcss(postcssPluginsPreset({
     next: true,
     minify: true,
-  })).process(validInputCss, {
+  })).process(inputCss, {
     from: undefined,
   }).then(({ css }) => postcss([
     require('postcss-prettify'),
